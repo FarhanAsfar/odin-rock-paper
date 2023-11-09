70a6477dda3cb3699ab1b48ac8f3a2a055f6f20a
Arnob <farhanasfar21@gmail.com>
2023-11-10 05:03:34 +0600

    Add 'playRound' funciton to play the game

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-
-
 function getComputerChoice(){
     const choices = ['rock', 'paper', 'scissor'];
-    const randomIndex = Math.floor(Math.random() * words.length);
+    const randomIndex = Math.floor(Math.random() * choices.length);
 
-    const randomChoice = words[randomChoice];
+    const randomChoice = choices[randomIndex];
 
     return randomChoice;
 }
+
+function playRound(playerChoice, computerChoice){
+    if(playerChoice=='rock' && computerChoice=='paper'){
+        return 'you lose';
+    }
+    else if(playerChoice=='rock' && computerChoice=='scissor'){
+        return 'you won';
+    }
+    else if(playerChoice=='rock' && computerChoice=='rock'){
+        return 'game draw';
+    }
+}
+
+
 
 const playerChoice = 'rock';
 const computerChoice = getComputerChoice();
